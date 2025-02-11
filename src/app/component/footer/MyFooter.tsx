34be3bf9bf8abe42f3ae7600da4083095ddcb765
Matteo Garza <matteo.garza@outlook.it>
2025-02-11 23:27:53 +0100

    feat(main): component for footer

---
+++ src/app/component/footer/MyFooter.tsx
@@ -0,0 +1,16 @@
+import Link from 'next/link'
+
+export const MyFooter: React.FC = () => (
+  <p>
+    Created with 🐾 by
+    {' '}
+    <Link
+      style={{ textDecoration: 'none', color: '#eee', fontWeight: 'b' }}
+      href="https://github.com/CoachGodzup"
+      rel="noreferrer noopener"
+      target="_blank"
+    >
+      CoachGodzup
+    </Link>
+  </p>
+)
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,45 +3,33 @@
 import { AppShell, Container } from '@mantine/core'
 import Link from 'next/link'
 import React, { Suspense } from 'react'
+import { MyFooter } from './component/footer/MyFooter'
 import CardList from './component/list/cardList'
 import { Creator } from './component/modal/creator/creator'
 import { Providers } from './providers'
 
 const App: React.FC = () => {
   return (
-    <Suspense fallback="loading...">
-      <Providers>
-        <AppShell header={{ height: 60 }} padding="md">
-          <AppShell.Header>
-            <h3 style={{ paddingLeft: '1em' }}>Tickety</h3>
-          </AppShell.Header>
+    <Providers>
+      <AppShell header={{ height: 60 }} padding="md">
+        <AppShell.Header>
+          <h3 style={{ paddingLeft: '1em' }}>Tickety</h3>
+        </AppShell.Header>
 
-          <AppShell.Main>
-            <Container fluid>
-              <Suspense fallback="loading...">
-                <CardList></CardList>
-              </Suspense>
-            </Container>
-            <Creator />
-          </AppShell.Main>
+        <AppShell.Main>
+          <Container fluid>
+            <Suspense fallback="loading...">
+              <CardList></CardList>
+            </Suspense>
+          </Container>
+          <Creator />
+        </AppShell.Main>
 
-          <AppShell.Footer bg="#111" c="#eee" ta="center" pt={{}}>
-            <p>
-              Created with 🐾 by
-              {' '}
-              <Link
-                style={{ textDecoration: 'none', color: '#eee', fontWeight: 'b' }}
-                href="https://github.com/CoachGodzup"
-                rel="noreferrer noopener"
-                target="_blank"
-              >
-                CoachGodzup
-              </Link>
-            </p>
-          </AppShell.Footer>
-        </AppShell>
-      </Providers>
-    </Suspense>
+        <AppShell.Footer bg="#111" c="#eee" ta="center" pt={{}}>
+          <MyFooter></MyFooter>
+        </AppShell.Footer>
+      </AppShell>
+    </Providers>
   )
 }
 
