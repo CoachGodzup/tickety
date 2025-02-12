6e9f18de29d3f0715a969efd0cb2c42d20f8b1ec
Matteo Garza <matteo.garza@outlook.it>
2025-02-12 02:41:02 +0100

    fix(main): fix style

--- src/app/component/navbar/navbar.tsx
+++ src/app/component/navbar/navbar.tsx
@@ -1,10 +1,11 @@
-import { Stack } from '@mantine/core'
+import { Stack, Title } from '@mantine/core'
 import Sort from './sort'
 import StatDoneNotDone from './statDoneNotDone'
 
 export const Navbar: React.FC = () => (
   <Stack>
     <StatDoneNotDone></StatDoneNotDone>
+    <Title size="xl">Actions</Title>
     <Sort></Sort>
   </Stack>
 )
