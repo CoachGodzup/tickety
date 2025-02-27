40595fa46bf140f39a6014d3bdc9dfe10300fee0
Matteo Garza <matteo.garza@outlook.it>
2025-02-27 12:35:25 +0100

    chore(main): added vercel speed insights

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import Main from './main'
 import { Providers } from './providers'
+import { SpeedInsights } from "@vercel/speed-insights/next"
+
 
 const App: React.FC = () => {
   return (
     <Providers>
+      <SpeedInsights />
       <Main></Main>
     </Providers>
   )
