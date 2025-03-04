d4d55263fad67e2238a91862c338cea80edf0f14
Matteo Garza <matteo.garza@outlook.it>
2025-03-05 00:28:33 +0100

    feat(feature/tags): added tag editor

--- src/model/card.ts
+++ src/model/card.ts
@@ -1,6 +1,10 @@
+import { MantineColor } from "@mantine/core"
+
 export interface Card {
   readonly id: string
   title: string
   body: string
   isDone: boolean
+  color?: MantineColor
+  badges: string[]
 }
