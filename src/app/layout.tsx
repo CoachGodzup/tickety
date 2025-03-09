578c43677117dc81a3d2991834b64679035aafbd
Matteo Garza <matteo.garza@outlook.it>
2025-03-09 01:44:40 +0100

    feat(feature/tags): stat  badges in navbar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,6 +29,7 @@ export default function RootLayout({
   return (
     <html lang="en" data-mantine-color-scheme="dark">
       <head>
+        <title>Tickety</title>
         <meta charSet="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <link rel="icon" href="/favicon.ico" />
--- src/component/navbar/navbar.tsx
+++ src/component/navbar/navbar.tsx
@@ -2,10 +2,12 @@ import { Paper, Stack, Title } from '@mantine/core'
 import Sort from './sort'
 import StatDoneNotDone from './statDoneNotDone'
 import { AllActions } from './allActions'
+import StatBadges from './statBadges'
 
 export const Navbar: React.FC = () => (
   <Stack>
     <StatDoneNotDone></StatDoneNotDone>
+    <StatBadges></StatBadges>
     <Title size="xl">Actions</Title>
     <Sort></Sort>
     <Paper hiddenFrom='sm'>
