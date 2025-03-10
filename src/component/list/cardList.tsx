fb28d8a3cf5385ce923a6b09cfb5ff5e6511d1eb
Matteo Garza <matteo.garza@outlook.it>
2025-03-10 02:17:09 +0100

    Feat(feature/tags): added Zustand, filtering via tags

--- src/component/list/cardList.tsx
+++ src/component/list/cardList.tsx
@@ -3,22 +3,33 @@
 import type { Card } from '../../model/card'
 import type { RootState } from '../../store/root.store'
 import { Flex } from '@mantine/core'
-import React, { Suspense, useMemo } from 'react'
+import React, { Suspense, useEffect, useMemo } from 'react'
 import { useSelector } from 'react-redux'
 import CardComponent from '../card/cardComponent'
+import { useFilterStore } from '@/store/filter.store.zustand'
 
 const CardList: React.FC = () => {
-  const cards: Card[] = useSelector((state: RootState) => state.cards.cards)
+  const cards: Card[] = useSelector((state: RootState) => state.cards).cards
+  const filterList = useFilterStore((state) => state.filterList)
 
+  useEffect(() => {
+    console.log('CardList rendered', cards)
+  }, [cards])
+    
   const cardListElements = useMemo(
-    () => cards.map((card) => <CardComponent key={card.id} card={card} />),
-    [cards]
+    () => cards
+      .filter((card) => filterList.length === 0 || filterList.every(filter => card.badges?.includes(filter)))
+      .map((card) => <CardComponent key={card.id} card={card} />),
+    [cards, filterList]
   )
 
   return (
-    <Flex gap="md" rowGap="md" columnGap="md" wrap="wrap" direction="row">
-      {cardListElements}
-    </Flex>
+    <Suspense fallback={<div>Loading...</div>}>
+      {cardListElements.length === 0 && <div>No cards found</div>}
+      <Flex gap="md" rowGap="md" columnGap="md" wrap="wrap" direction="row">
+        {cardListElements}
+      </Flex>
+    </Suspense>
   )
 }
 
---
+++ src/store/filter.store.zustand.ts
@@ -0,0 +1,18 @@
+import { Card } from '@/model/card';
+import {create} from 'zustand';
+
+type State = {
+    filterList: string[],
+};
+
+type Actions = {
+    toggleFilter: (filter: string) => void,
+}
+
+export const useFilterStore = create<State & Actions>((set) => ({
+    filterList: [] as string[],
+    toggleFilter: (filter: string) => set((state) => ({
+        ...state,
+        filterList: state.filterList.includes(filter) ? state.filterList.filter((f) => f !== filter) : [...state.filterList, filter],
+    })),
+}));
